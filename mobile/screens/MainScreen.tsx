db13b38a4e05e550c5c9194327297077efc4458f
Olli Kiikkilä <15906493+ollisterr@users.noreply.github.com>
2021-11-20 00:39:56 +0200

    MainScreen content wrapping

--- mobile/screens/MainScreen.tsx
+++ mobile/screens/MainScreen.tsx
@@ -1,36 +1,43 @@
 import * as React from 'react';
 
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import styled from '../styles';
 import { Row, ScreenWrapper, Spacer } from '../styles/components';
 import { H1, Text, Detail } from '../styles/typography';
 import background from '../assets/images/rain-drops.jpg';
 import DropSVG from '../assets/images/drop-icon.svg';
+import LogoSVG from '../assets/images/drop-logo.svg';
 
 export default function MainScreen() {
+  const insets = useSafeAreaInsets();
   return (
     <ScreenWrapper>
       <BackgroundImage source={background} />
 
       <DropIcon />
 
-      <HeaderStats>
-        <H1 color="primary">20,5l</H1>
+      <Content insets={insets}>
+        <LogoIcon width={120} />
 
-        <Detail>liters of water</Detail>
-        <Detail>consumed today</Detail>
+        <HeaderStats>
+          <H1 color="primary">20,5l</H1>
 
-        <Spacer axis="y" />
+          <Detail>liters of water</Detail>
+          <Detail>consumed today</Detail>
 
-        <Row justify="center">
-          <Text color="success" weight="bold">
-            -10%
-          </Text>
+          <Spacer axis="y" />
 
-          <Spacer spacing="xxsmall" />
+          <Row justify="center">
+            <Text color="success" weight="bold">
+              -10%
+            </Text>
 
-          <Text>from last week</Text>
-        </Row>
-      </HeaderStats>
+            <Spacer spacing="xxsmall" />
+
+            <Text>from last week</Text>
+          </Row>
+        </HeaderStats>
+      </Content>
     </ScreenWrapper>
   );
 }
@@ -46,15 +53,22 @@ const DropIcon = styled(DropSVG)`
   position: absolute;
   z-index: -1;
   align-self: center;
-  width: 80%;
   top: 0;
   color: ${p => p.theme.colors.primaryLight};
   opacity: 0.15;
+`;
+
+const LogoIcon = styled(LogoSVG)`
+  align-self: center;
+`;
+
+const Content = styled.View<{ insets: { top: number; bottom: number } }>`
+  padding-top: ${p => p.insets.top}px;
+  padding-bottom: ${p => p.insets.bottom}px;
 `;
 
 const HeaderStats = styled.View`
   width: 70%;
   align-self: center;
   align-items: center;
-  margin-top: 20%;
 `;
