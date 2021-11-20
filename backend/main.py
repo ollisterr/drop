8a0bc3b6d83049e36c48ae0ef0dee34e4a4fe61a
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-11-20 18:25:15 +0200

    feat: remove uvicorn reload in prod

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,9 @@ load_dotenv()
 
 PORT = int(os.getenv("PORT", 8000))
 HOST = os.getenv("HOST", "0.0.0.0")
+ENV = os.getenv("ENV", "development")
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app:app", host=HOST, port=PORT, reload=True)
+    uvicorn.run("app:app", host=HOST, port=PORT, reload=True if ENV == "development" else False)
