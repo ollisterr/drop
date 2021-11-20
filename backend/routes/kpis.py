0418f9dcecd2b0dcec87f8c5fe75e6a1145ade20
Joni Tarkiainen <joni.m.tarkiainen@gmail.com>
2021-11-21 00:03:23 +0200

    weekly trend endpoint

--- backend/routes/kpis.py
+++ backend/routes/kpis.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from orm.tables import Apartment, User
+from orm.tables import Apartment, User, Measurement
 
 router = APIRouter()
 
@@ -12,3 +12,18 @@ async def get_user_daily_consumption(username):
     apartment = Apartment.select()
     print(user)
     return user
+
+
+@router.get("/weekly-change/{apartment}", tags=["kpi"])
+async def get_user_weekly_trend(apartment: int):
+    two_week = Measurement.raw(
+        "SELECT DATE(timestamp), SUM(water_consumption) FROM Measurement WHERE Apartment = {} GROUP BY DATE(timestamp) LIMIT(14)",
+        apartment,
+    ).run_sync()
+    first_sum = 0
+    second_sum = 0
+    for i in range(0, 7):
+        first_sum += two_week[i]["sum"]
+    for i in range(7, 14):
+        second_sum += two_week[i]["sum"]
+    return round(1 - first_sum / second_sum, 2) * 100
