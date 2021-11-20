ec3a4eeb29f17a7ea02231c09129430573ccd2c4
Timo Riski <25483483+rriski@users.noreply.github.com>
2021-11-20 18:30:20 +0200

    fix: prod fixes

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ cd /srv/www/drop
 git pull
 
 cd backend && make env
+cd backend && sed -i s/development/production/g .env
 docker volume create --name=db_volume
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
